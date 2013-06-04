6b497542f1b257cd44242d86865b16b47ef8e46f
Rui Zhuang <zrui@ksu.edu>
2013-06-04 08:54:32 -0500

    query_asset.php

--- query_asset.php
+++ query_asset.php
@@ -5,21 +5,20 @@ error_reporting(E_ALL);
 
 require_once 'conn.php';
 
-echo "load login";
 $asset_server = mysql_connect($asset_host, $db_username, $db_password);
 
 if (!$asset_server) die("failed to connect mysql: " . mysql_error() );
 
 mysql_select_db($asset_db) or die("unable to select database:" . mysql_error() );
 
-$query = "SELECT * FROM target";
+$query = "SELECT * FROM $asset_db";
 $result = mysql_query($query);
 
 if (!$result) die("Database access failed: " . mysql_error() );
 
 $rows = mysql_num_rows($result);
 echo $rows;
-echo "<table><tr> <th>id</th> <th>name</th> <th>xloc</th> <th>yloc</th></tr>";
+echo "<table align = 'center'><tr> <th>id</th> <th>name</th> </tr>";
 
 echo "</table>";
 
--- query_geo.php
+++ query_geo.php
@@ -5,22 +5,20 @@ error_reporting(E_ALL);
 
 require_once 'conn.php';
 
-echo "load login";
 $geo_server = mysql_connect($geo_host, $db_username, $db_password);
 
 if (!$geo_server) die("failed to connect mysql: " . mysql_error() );
 
 mysql_select_db($geo_db) or die("unable to select database:" . mysql_error() );
 
-$query = "SELECT * FROM target";
+$query = "SELECT * FROM $geo_db";
 $result = mysql_query($query);
 
 if (!$result) die("Database access failed: " . mysql_error() );
 
 $rows = mysql_num_rows($result);
 echo $rows;
-echo "<table><tr> <th>id</th> <th>name</th> <th>xloc</th> <th>yloc</th></tr>";
-
+echo "<table><tr> <th>id</th> <th>name</th> </tr>";
 echo "</table>";
 
 ?>
